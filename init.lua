252d63233bab34d26fb7830123041b9240780c65
Edwin Wenink <edwinwenink@hotmail.com>
2024-07-06 16:01:32 +0200

    tpope essentials

--- init.lua
+++ init.lua
@@ -137,7 +137,7 @@ vim.opt.timeoutlen = 300
 
 -- Configure how new splits should be opened
 vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.splitbelow = false
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
@@ -229,6 +229,8 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-fugitive', -- Git integration
+  'tpope/vim-vinegar', -- Beter netrw experience
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
